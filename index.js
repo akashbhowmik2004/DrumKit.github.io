a770708bd884dbeb64358c796b38583a271bf1e6
akashbhowmik2004 <bhowmikakash035@gmail.com>
2025-08-05 12:31:04 +0530

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 document.querySelector(".w").addEventListener("keydown" , (e) => {
     var key = e.key;
-    var audio1 = new Audio('sounds/tom-1.mp3');
-    var audio2 = new Audio('sounds/tom-2.mp3');
-    var audio3 = new Audio('sounds/tom-3.mp3');
-    var audio4 = new Audio('sounds/tom-4.mp3');
-    var audio5 = new Audio('sounds/crash.mp3');
-    var audio6 = new Audio('sounds/kick-bass.mp3');
-    var audio7 = new Audio('sounds/snare.mp3');
+    var audio1 = new Audio('tom-1.mp3');
+    var audio2 = new Audio('tom-2.mp3');
+    var audio3 = new Audio('tom-3.mp3');
+    var audio4 = new Audio('tom-4.mp3');
+    var audio5 = new Audio('crash.mp3');
+    var audio6 = new Audio('kick-bass.mp3');
+    var audio7 = new Audio('snare.mp3');
     switch(key){
         case 'w':
             audio1.play();
@@ -33,4 +33,5 @@ document.querySelector(".w").addEventListener("keydown" , (e) => {
         
     }
 })
+
     
